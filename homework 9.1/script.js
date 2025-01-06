3732cb8b8b7fc22aed609203b14783e18db02d3d
vviktwin <v.ch12022009@gmail.com>
2025-01-06 19:33:18 +0200

    Add files via upload

---
+++ homework 9.1/script.js
@@ -0,0 +1,7 @@
+let counter = 0
+while(10 >= counter) {
+    const result = 5 * counter;
+    console.log("5 * " + counter + " = " + result);
+    counter = counter + 1;
+}
+console.log("Обчислення завершено!!!")
---
+++ homework 9.2/script.js
@@ -0,0 +1,9 @@
+function convertValue(uah) {
+    const result = uah / 42
+    console.log(result)
+    console.log("У вас є " + uah + " гривень, якщо ви хочете купити доллар, то за 2500 ви отримаєте " + result)
+}
+
+convertValue(2500)
+convertValue(5700)
+convertValue(280000)
